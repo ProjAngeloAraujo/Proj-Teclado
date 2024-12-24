3d43abbf1db3748b5218819bf9b57b42e3b804b1
ProjAngeloAraujo <angelo.souza23@etec.sp.gov.br>
2024-12-24 17:03:23 -0300

    dia 2

--- java.js
+++ java.js
@@ -1,10 +1,10 @@
-function tecla(elemento){
+function tecla(elemento) {
     const valor = elemento.getAttribute('value');
-    document.getElementById("input").value = document.getElementById("input").value + valor;
-
+    const input = document.getElementById("input"); 
+    input.innerText = input.innerText + valor;  
 }
 
-function del(){
-    const input = document.getElementById("input");
-    input.value = input.value.slice(0, -1);
+function del() {
+    const input = document.getElementById("input"); 
+    input.innerText = input.innerText.slice(0, -1); 
 }
